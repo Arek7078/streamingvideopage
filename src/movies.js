160d6f34e43293f5e83b7bf1dbd5f68df6c18c06
Arek908 <arekkasz07@gmail.com>
2025-08-10 15:17:21 +0100

    movies.js //function to display the most popular movie from the movies.json file

--- src/movies.js
+++ src/movies.js
@@ -70,27 +70,43 @@
     export async function mostPopular(){
       const response = await fetch('./movies.json');
       const data = await response.json();
-      // console.log(data);
-      const movieContainer = document.getElementById('most-popular')
-      let ocena = null
-      for(let key in data){
-        data[key].forEach(movie => {
-          // console.log(movie.ocena)
-          
-          if(movie.ocena > ocena){
-            ocena = movie
-            console.log(ocena)
-            
+
+
+      let scoreArr = []
+      let highest = 0
+      let bestMovie = null
+      for(const category of Object.values(data)){
+        // console.log(category)
+        for(const key of category){
+          // console.log(key.ocena)
+          scoreArr.push(key.ocena)
+           highest = Math.max(...scoreArr);
+          if(key.ocena === highest){
+            bestMovie = key
           }
-          
-          movieContainer.innerHTML = `<div class = 'bg-gray-950 rounded-lg border-2 border-gray-400 '><img src="${ocena.obraz}" alt="${ocena.tytul}" class="w-full h-full object-fill rounded">
-          <h3 class="font-bold text-xs md:text-lg mt-2 ">${ocena.tytul}</h3>
-          <p class="font-bold text-yellow-400 text-xs md:text-lg  ">Ocena: ${ocena.ocena}</p></div>`;
-        })
-        
+        }
+      }
+      // console.log(bestMovie)
+      const movieContainer = document.getElementById('most-popular')
+      // console.log(movieContainer)
+      if(window.innerWidth < 768){
+      movieContainer.innerHTML =
+      `<div class="w-full h-screen pb-25 md:hidden">
+      <img src = ${bestMovie.obraz} class="w-full h-full object-fill rounded-2xl"
+      >
+      <h3 class="
+      text-2xl md:text-4xl font-bold text-white mt-4 text-center  shadow-2xl 
+      ">${bestMovie.tytul}</h3>
+      <p class="
+      text-lg md:text-2xl font-semibold text-yellow-400 text-center  shadow-2xl 
+      ">Ocena: ${bestMovie.ocena}</p>
+      </div>`
+      }else{
+        const trailer = bestMovie.trailer + '?autoplay=1&mute=1';
+        console.log(trailer)
+        movieContainer.innerHTML =`<iframe class="w-full h-screen" src=${trailer} frameborder="0" autoplay allowfullscreen></iframe>`
       }
       
-        
-
+      
     }
     
